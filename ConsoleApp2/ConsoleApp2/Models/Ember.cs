4ce69d34c998095c6fcbbe58a90ade59eede2905
Lacilo <144881417+Lacilo@users.noreply.github.com>
2025-12-12 11:03:28 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Models/Ember.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Models
+{
+    class Ember
+    {
+        int id;
+        string nev;
+        int pont1;
+        float ido1;
+        int pont2;
+        float ido2;
+        int pont3;
+        float ido3;
+        float idoL;
+        int pontL;
+
+        public Ember(int id, string nev, int pont1, float ido1, int pont2, float ido2, int pont3, float ido3, float idoL, int pontL)
+        {
+            this.id = id;
+            this.nev = nev;
+            this.pont1 = pont1;
+            this.ido1 = ido1;
+            this.pont2 = pont2;
+            this.ido2 = ido2;
+            this.pont3 = pont3;
+            this.ido3 = ido3;
+            this.idoL = idoL;
+            this.pontL = pontL;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public int Pont1 { get => pont1; set => pont1 = value; }
+        public float Ido1 { get => ido1; set => ido1 = value; }
+        public int Pont2 { get => pont2; set => pont2 = value; }
+        public float Ido2 { get => ido2; set => ido2 = value; }
+        public int Pont3 { get => pont3; set => pont3 = value; }
+        public float Ido3 { get => ido3; set => ido3 = value; }
+        public float IdoL { get => idoL; set => idoL = value; }
+        public int PontL {  get => pontL; set => pontL = value; }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using ConsoleApp2.Models;
+using System.Data.SqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            DisplayAllUsers();
+
+        }
+
+        // ADATBÁZIST BETÖLTŐ FÜGGVÉNY
+        static List<Ember> readDB() 
+        {
+
+
+            return new List<Ember> { };
+        }
+
+        static float GetMin(float i1, float i2, float i3)
+        {
+            float[] times = { i1, i2, i3 };
+            float min = times[0];
+
+            foreach (float t in times) 
+            {
+                if (t < min)
+                {
+                    min = t;
+                }
+            }
+
+            return min;
+        }
+        static int GetMax(int i1, int i2, int i3)
+        {
+            int[] times = { i1, i2, i3 };
+            int max = times[0];
+
+            foreach (int t in times)
+            {
+                if (t > max)
+                {
+                    max = t;
+                }
+            }
+
+            return max;
+        }
+
+        static void DisplayAllUsers()
+        {
+            MySqlConnection connection = new MySqlConnection();
+            string connectionString = "SERVER = localhost; DATABASE = szbk_verseny; UID = root; PASSWORD = ;";
+            connection.ConnectionString = connectionString;
+            connection.Open();
+
+
+            string sql = "SELECT * FROM versenyzok";
+            MySqlCommand cmd = new MySqlCommand(sql, connection);
+            List<Ember> users = new List<Ember>();
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Ember user = new Ember(
+                    id: reader.GetInt32("Id"),
+                    nev: reader.GetString("Nev"),
+                    pont1: reader.GetInt32("Pont1"),
+                    ido1: reader.GetFloat("Ido1"),
+                    pont2: reader.GetInt32("Pont2"),
+                    ido2: reader.GetFloat("Ido2"),
+                    pont3: reader.GetInt32("Pont3"),
+                    ido3: reader.GetFloat("Ido3"),
+                    idoL: reader.GetFloat("IdoL"),
+                    pontL: reader.GetInt32("PontL")
+                    );
+
+                user.IdoL = GetMin(user.Ido1, user.Ido2, user.Ido3);
+                user.PontL = GetMax(user.Pont1, user.Pont2, user.Pont3);
+
+                users.Add(user);
+            }
+
+            connection.Close();
+
+            foreach (Ember user in users)
+            {
+                Console.Write(user.Id + "|");
+                Console.Write(user.Nev + "|");
+                Console.Write(user.Pont1 + "|");
+                Console.Write(user.Pont2 + "|");
+                Console.Write(user.Pont3 + "|   ");
+                Console.Write(user.PontL + "|");
+                Console.Write(user.IdoL);                
+                Console.WriteLine();
+            }
+        }
+    }
+}
